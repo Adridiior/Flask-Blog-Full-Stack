b15557b7385222b85278965480eab74197790c54
Adridiior <175614943+Adridiior@users.noreply.github.com>
2026-02-06 16:20:29 +0100

    fix: replace main.index with main.home in auth routes

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -21,7 +21,7 @@ def register():
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('main.index'))
+        return redirect(url_for('main.home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
