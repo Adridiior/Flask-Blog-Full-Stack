7be62bb6f0846071488db19424e4a9f809e478a0
Adridiior <175614943+Adridiior@users.noreply.github.com>
2026-02-06 16:15:23 +0100

    fix: redirect to main.home after login and in 500 page

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -28,7 +28,7 @@ def login():
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
             next_page = request.args.get('next')
-            return redirect(next_page) if next_page else redirect(url_for('main.index'))
+            return redirect(next_page) if next_page else redirect(url_for('main.home'))
         else:
             flash('Login unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
